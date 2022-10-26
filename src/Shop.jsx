81b19634009f46211e9c0e7a447566ff8b63d397
M Anas Latif <m.anaslatif.com@gmail.com>
2022-10-26 12:02:55 +0500

    video 74

---
+++ src/Shop.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Shop() {
+    return (
+        <div>
+            <h2>Buy Adidas Shoes 50 Rs</h2>
+            <button className='btn btn-primary'>-</button>
+            Add This item to card
+            <button className='btn btn-primary'>+</button>
+        </div>
+    )
+}
